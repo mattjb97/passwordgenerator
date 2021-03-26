18f1df1f909d6b3b64a1434761dc812225d2d266
Mattjb97 <bohlmatthew@gmail.com>
2021-03-25 22:33:41 -0500

    adding variables for numbers letters and characters

--- assests/script.js
+++ assests/script.js
@@ -1,15 +1,23 @@
 // Assignment Code
 
 //my code goes here 
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var Lower = ["abcdefghijklmnopqrstuvwxyz"];
+var Upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var Numbers = [1234567890];
+var Characters = ["!@#$%^&*()_+={[}]';:?><"];
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 //no need to touch
